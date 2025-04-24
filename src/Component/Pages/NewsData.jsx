28f99168ddbc95a4b7cec0c90ff494d6ad29789c
CodeWithLeonard <leonardskargbo@gmail.com>
2025-04-24 11:38:27 +0000

    second

--- src/Component/Pages/NewsData.jsx
+++ src/Component/Pages/NewsData.jsx
@@ -1,7 +1,7 @@
 const newsData = [
   {
     id: 1,
-    title: "𝐅𝐔𝐋𝐋-𝐓𝐈𝐌𝐄",
+    title: "FULL TIME",
     date: "April 19, 2025",
     image: "/fulltime.jpg",
     content: `
@@ -11,10 +11,9 @@ We continue our impressive run with a commanding victory at home. Clinical finis
 Goals:
 • Mohamadu Lamin (2 goals)
 • Bailor Bah (penalty)
-The champions are showing why they’re still the team to beat!
+The champions are showing why they're still the team to beat!
     `
-  }
-  ,
+  },
   {
     id: 2,
     title: "final Training",
@@ -23,14 +22,14 @@ The champions are showing why they’re still the team to beat!
     content: `
 Bo Rangers Wrap Up Final Training Ahead of Crucial Clash Against Bai Bureh Warriors*
 Bo Rangers have completed their final training session at the Southern Arena ahead of tomorrow’s highly anticipated match against Bai Bureh Warriors. The players looked sharp and focused, with high spirits and determination evident throughout the session.
-Tomorrow’s fixture marks the final match of the Leone Rock Premier League first round, and the team is fully committed to finishing on a high note with a win.`
+Tomorrow's fixture marks the final match of the Leone Rock Premier League first round, and the team is fully committed to finishing on a high note with a win.`
   },
   {
     id: 3,
     title: "Coach Musu Kamara Talks Season Ambitions",
     date: "April 15, 2025",
     image: "/staff1.jpg",
-    content: `In an exclusive interview, head coach Musu Kamara discusses the team’s preparation and goals for the season. He praised the commitment of the players and the support from fans. He also hinted at a few tactical changes and new signings...`
+    content: `In an exclusive interview, head coach Musu Kamara discusses the team's preparation and goals for the season. He praised the commitment of the players and the support from fans. He also hinted at a few tactical changes and new signings...`
   }
 ];
 
